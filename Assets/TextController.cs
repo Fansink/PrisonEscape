05bdfb16ca842a226502dda1105f37b9c10abb79
Fansink <flansink@kza.nl>
2016-10-27 17:31:54 +0200

    Eerste scherm en keystrokes toegevoegd
    Scherm zwart gemaakt
    Tekst toegevoegd
    Actie op indrukken spatiebalk toegevoegd

---
+++ Assets/TextController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TextController : MonoBehaviour {
+
+    public Text text;
+
+	// Use this for initialization
+	void Start () {
+        text.text = "Hallo Wereld";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.Space))
+        print (text.text = "Spatie is ingedrukt");
+    }
+}
